f797121d944543defceaf2c04675753a92eefd14
Guilherme Miranda <guilherme_de_oliveira_n1@hotmail.com>
2024-02-22 16:30:06 -0300

    feat: add timestamps to Product model

--- admin-panel/models/Product.ts
+++ admin-panel/models/Product.ts
@@ -1,12 +1,17 @@
 import mongoose, { Schema, model, models } from "mongoose";
 
-const ProductSchema = new Schema({
-  title: { type: String, required: true },
-  category: { type: mongoose.Types.ObjectId, ref: "Category" },
-  properties: { type: Object },
-  description: String,
-  price: { type: Number, required: true },
-  images: [{ type: String }],
-});
+const ProductSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    category: { type: mongoose.Types.ObjectId, ref: "Category" },
+    properties: { type: Object },
+    description: String,
+    price: { type: Number, required: true },
+    images: [{ type: String }],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export const Product = models.Product || model("Product", ProductSchema);
